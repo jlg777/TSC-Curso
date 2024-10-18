c62558efc9f3fc3c98187070aa08daaa1480f953
= <=>
2024-10-18 14:45:32 -0300

    m05

--- Modulo_05/class.js
+++ Modulo_05/class.js
@@ -1,15 +1,26 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Car = /** @class */ (function () {
     // Constructor
     function Car(make, color, doors) {
         if (doors === void 0) { doors = 4; }
         this._make = make;
         this._color = color;
-        if ((doors % 2) === 0) {
-            this._doors = doors;
-        }
-        else {
-            throw new Error('Doors must be an even number');
-        }
+        this._doors = doors;
+        Car.numberOfCars++; // Increments the value of the static property
     }
     Object.defineProperty(Car.prototype, "make", {
         // Accessors
@@ -61,10 +72,51 @@ var Car = /** @class */ (function () {
     Car.prototype.worker = function () {
         return this._make;
     };
+    Car.getNumberOfCars = function () {
+        return Car.numberOfCars;
+    };
+    // Properties
+    Car.numberOfCars = 0; // New static property
     return Car;
 }());
-var myCar1 = new Car('Galaxy Motors', 'gray');
-//console.log(myCar3.doors);  // returns 4, the default value
-console.log(myCar1.accelerate(35));
-console.log(myCar1.brake());
-console.log(myCar1.turn('right'));
+// Instantiate the Car object with all parameters
+var myCar1 = new Car('Cool Car Company', 'blue', 2);
+// Instantiates the Car object with all parameters
+var myCar2 = new Car('Galaxy Motors', 'blue', 2);
+// Returns 2
+/*console.log(Car.getNumberOfCars());
+console.log(myCar1.color)*/
+//const resultado = myCar1.accelerate(5);
+var ElectricCar = /** @class */ (function (_super) {
+    __extends(ElectricCar, _super);
+    // Constructor
+    function ElectricCar(make, color, range, doors) {
+        if (doors === void 0) { doors = 2; }
+        var _this = _super.call(this, make, color, doors) || this;
+        _this._range = range;
+        return _this;
+    }
+    Object.defineProperty(ElectricCar.prototype, "range", {
+        // Accessors
+        get: function () {
+            return this._range;
+        },
+        set: function (range) {
+            this._range = range;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    // Methods
+    ElectricCar.prototype.charge = function () {
+        console.log(this.worker() + " is charging.");
+    };
+    return ElectricCar;
+}(Car));
+var spark = new ElectricCar('Spark Motors', 'silver', 124, 2);
+var eCar = new ElectricCar('Electric Car Co.', 'black', 263);
+//console.log(eCar.doors);         // returns the default, 2
+//spark.charge();                 // returns "Spark Motors is charging"
+spark.accelerate(5);
+var resultado = spark.brake();
+console.log(resultado);
--- Modulo_05/class.ts
+++ Modulo_05/class.ts
@@ -1,67 +1,106 @@
-class Car {
-  // Properties
-  _make: string
-  _color: string
-  _doors: number
+  class Car {
+    // Properties
+    private static numberOfCars: number = 0;  // New static property
+    private _make: string;
+    private _color: string;
+    private _doors: number;
 
-  // Constructor
-  constructor(make: string, color: string, doors = 4) {
-    this._make = make;
-    this._color = color;
-    if ((doors % 2) === 0) {
+    // Constructor
+    constructor(make: string, color: string, doors = 4) {
+        this._make = make;
+        this._color = color;
         this._doors = doors;
-    } else {
-        throw new Error('Doors must be an even number');
+        Car.numberOfCars++; // Increments the value of the static property
     }
+    
+    // Accessors
+    get make() {
+      return this._make
+    }
+
+    set make(make) {
+      this._make = make
+    }
+
+    get color() {
+      return 'The color of the car is ' + this._color
+    }
+
+    set color(color) {
+      this._color = color
+    }
+
+    get doors() {
+      return this._doors
+    }
+    
+    set doors(doors) {
+      if (doors % 2 === 0) {
+        this._doors = doors
+      } else {
+        throw new Error('Doors must be an even number')
+      }
+    }
+
+    // Methods
+    accelerate(speed: number): string {
+      return `${this.worker()} is accelerating to ${speed} MPH.`
+    }
+    brake(): string {
+      return `${this.worker()} is braking with the standard braking system.`
+    }
+    turn(direction: 'left' | 'right'): string {
+      return `${this.worker()} is turning ${direction}`
+    }
+    // This function performs work for the other method functions
+    worker(): string {
+      return this._make
+    }
+
+    public static getNumberOfCars(): number {
+      return Car.numberOfCars;
+  }
+  }
+
+// Instantiate the Car object with all parameters
+let myCar1 = new Car('Cool Car Company', 'blue', 2);
+// Instantiates the Car object with all parameters
+let myCar2 = new Car('Galaxy Motors', 'blue', 2);
+// Returns 2
+/*console.log(Car.getNumberOfCars());
+console.log(myCar1.color)*/
+//const resultado = myCar1.accelerate(5);
+
+class ElectricCar extends Car {
+  // Properties
+private _range: number;
+  // Constructor
+constructor(make: string, color: string, range: number, doors = 2) {
+  super(make, color, doors);
+  this._range = range;
+}
+ // Accessors
+get range() {
+  return this._range;
+}
+set range(range) {
+  this._range = range;
 }
 
-  // Accessors
-  get make() {
-    return this._make
-  }
+ // Methods
+charge() {
+  console.log(this.worker() + " is charging.")
+}
+/*accelerate(speed: number): string {
+  return `${this.worker()} is accelerating to ${speed} MPH.`
+}*/
 
-  set make(make) {
-    this._make = make
-  }
-
-  get color() {
-    return 'The color of the car is ' + this._color
-  }
-  set color(color) {
-    this._color = color
-  }
-
-  get doors() {
-    return this._doors
-  }
-  set doors(doors) {
-    if (doors % 2 === 0) {
-      this._doors = doors
-    } else {
-      throw new Error('Doors must be an even number')
-    }
-  }
-
-  // Methods
-  accelerate(speed: number): string {
-    return `${this.worker()} is accelerating to ${speed} MPH.`
-  }
-  brake(): string {
-    return `${this.worker()} is braking with the standard braking system.`
-  }
-  turn(direction: 'left' | 'right'): string {
-    return `${this.worker()} is turning ${direction}`
-  }
-  // This function performs work for the other method functions
-  worker(): string {
-    return this._make
-  }
 }
 
-let myCar1 = new Car('Galaxy Motors', 'gray');
-//console.log(myCar3.doors);  // returns 4, the default value
-console.log(myCar1.accelerate(35));
-console.log(myCar1.brake());
-console.log(myCar1.turn('right'));
-console.log(myCar1.turn('right'));
-console.log(myCar1.turn('right'));
+let spark = new ElectricCar('Spark Motors','silver', 124, 2);
+let eCar = new ElectricCar('Electric Car Co.', 'black', 263);
+//console.log(eCar.doors);         // returns the default, 2
+//spark.charge();                 // returns "Spark Motors is charging"
+ spark.accelerate(5);  
+ const resultado = spark.brake();
+console.log(resultado); 
